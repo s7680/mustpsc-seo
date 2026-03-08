7ca49709cc7743f1f8a7e58e3cfb506d94f15119
s7680 <suman7680@gmail.com>
2026-03-08 07:18:13 +0530

    Create sitemap.xml.js

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,34 @@
+import supabase from '../lib/supabase'
+
+export async function getServerSideProps({ res }) {
+  const siteUrl = 'https://seo.mustpsc.in'
+
+  const { data: questions } = await supabase
+    .from('questions')
+    .select('slug')
+
+  const urls = questions
+    .map(q => `
+      <url>
+        <loc>${siteUrl}/mcq/${q.slug}</loc>
+      </url>
+    `)
+    .join('')
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+  </urlset>`
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {}
+  }
+}
+
+export default function Sitemap() {
+  return null
+}
