a98c872463bf41a583d7f2a8136f210f112321fc
s7680 <suman7680@gmail.com>
2026-03-08 07:14:36 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="-UsCSZp4Ul3WtJydVJHoJHyIGvm1MlpiyUAGT3Qpq6U"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
