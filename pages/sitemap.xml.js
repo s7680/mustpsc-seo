14ba69ab87a780b14c4d3e316167118336540c24
s7680 <suman7680@gmail.com>
2026-03-08 07:22:36 +0530

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,9 +3,26 @@ import supabase from '../lib/supabase'
 export const getServerSideProps = async ({ res }) => {
   const siteUrl = 'https://seo.mustpsc.in'
 
-  const { data: questions } = await supabase
-    .from('questions')
-    .select('slug')
+  let questions = []
+  let from = 0
+  const batchSize = 1000
+
+  while (true) {
+    const { data, error } = await supabase
+      .from('questions')
+      .select('slug')
+      .range(from, from + batchSize - 1)
+
+    if (error) break
+
+    if (!data || data.length === 0) break
+
+    questions = questions.concat(data)
+
+    if (data.length < batchSize) break
+
+    from += batchSize
+  }
 
   const urls = (questions || [])
     .map(q => `
